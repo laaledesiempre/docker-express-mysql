7381e1aa9014f31308a1197a8852375b3366387d
laaledesiempre <alejandrabotacura@outlook.com.ar>
2023-09-12 14:21:40 -0300

    types and dependencies checked on express server

--- express-mysql-server/index.ts
+++ express-mysql-server/index.ts
@@ -1,13 +1,17 @@
+<<<<<<< HEAD
 import {Express, Response, Request} from 'express'
 const bodyParser = require('body-parser') //TODO
 const mysql = require('mysql') //TODO
 const cors = require('cors') //TODO
+=======
+import express, { Response, Request } from 'express'
+import mysql, { MysqlError } from 'mysql'
+>>>>>>> 7b43dcc (types and dependencies checked on express server)
 
 // Constants
 
-const express: Express=requiere('express')
-const app=express()
-const PORT=3000
+const app = express()
+const PORT = 3000
 
 // Data Base Conection
 
@@ -21,15 +25,15 @@ const db = mysql.createPool({
 //------------------
 // Functions
 
-const defaultResponseDatabase = (err,result) => {
+const defaultResponseDatabase = (err: MysqlError | null, result: any, res: Response) => {
 
-  if (error) {
-      res.sendStatus(500)
-    }
-    else if (result) {
-      res.sendStatus(200)
-      console.log(result)
-    }
+  if (err) {
+    res.sendStatus(500)
+  }
+  else {
+    res.sendStatus(200)
+    console.log(result)
+  }
 }
 
 // End of functions
@@ -37,18 +41,18 @@ const defaultResponseDatabase = (err,result) => {
 
 // Database constructor
 
-(()=>{    
+(() => {
   db.query(
-      'CREATE TABLE characters (id int not null auto_increment,name VARCHAR(50),age INT, description VARCHAR(255), primary key (id))',
-       (err, result) => {
-        if (err) { console.log(err) } 
-  })
+    'CREATE TABLE characters (id int not null auto_increment,name VARCHAR(50),age INT, description VARCHAR(255), primary key (id))',
+    (err, _result) => {
+      if (err) { console.log(err) }
+    })
 })()
 
 //------------------
 //Endpoints
 
-app.get("/test",(req,res)=>{
+app.get("/test", (_req, res) => {
 
   res.send("Hi from here")
 
@@ -56,27 +60,27 @@ app.get("/test",(req,res)=>{
 
 // Adding Characters Endpoint
 
-app.post("/api/character", (req,res)=>{
-  
-  const {name, age, description} = req.body
-  
+app.post("/api/character", (req: Request, res: Response) => {
+
+  const { name, age, description } = req.body
+
   db.query(
     'INSERT INTO characters (name, age, description) values (? , ? , ?)',
     [name, age, description],
-    defaultResponseDatabase(err,result)
+    (err, result) => defaultResponseDatabase(err, result, res)
   )
 })
 
 // Updating Characters Endpoint
 
-app.put("/api/character", (req,res)=>{
+app.put("/api/character", (req, res) => {
 
-  const {id ,name, age, description} = req.body
+  const { id, name, age, description } = req.body
 
   db.query(
     'UPDATE characters SET name = ?, age = ?, description = ? WHERE id = ?',
-    [name, age, description ,id],
-    defaultResponseDatabase(err,result)
+    [name, age, description, id],
+    (err, result) => defaultResponseDatabase(err, result, res)
   )
 })
 
@@ -90,42 +94,42 @@ app.delete("/api/character",(req,res)=>{
 
 // Getting Single Character Data Endpoint
 
-app.get("/api/character/id/:id",(req,res)=>{
+app.get("/api/character/id/:id", (req, res) => {
 
-  const {id}= requestr.params
+  const { id } = req.params
 
   db.query(
     'select * from characters where id=?',
     [id], (err, result) => {
 
-    if (err) {
-      console.log(err) 
-    } else { 
-      res.json(result) 
-    }
+      if (err) {
+        console.log(err)
+      } else {
+        res.json(result)
+      }
 
-  })
+    })
 })
 
 // Get All
 
-app.get("/api/characters",(req,res)=>{
+app.get("/api/characters", (_req, res) => {
   db.query(
     'select * from characters',
     (err, result) => {
 
-    if (err) {
-      console.log(err) 
-    } else { 
-      res.json(result) 
-    }
+      if (err) {
+        console.log(err)
+      } else {
+        res.json(result)
+      }
 
-  })
+    })
 })
 
 //------------------
 // App Listen
 
-app.listen(PORT,()=>{
-    console.log(`server on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`server on port ${PORT}`)
 })
